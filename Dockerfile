833f926fdac1af59f2fbc7c666d1da2fb84bb2dc
Alexandr Kiriukha <alex@meerkat.loony.rocks>
2023-11-08 09:50:29 +0100

    Docker stuff

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ ENV PYTHONUNBUFFERED=1
 WORKDIR /django-tech-test
 COPY . /django-tech-test/
 RUN pip3 install -r requirements.txt
+ENTRYPOINT python manage.py runserver 0.0.0.0:8000
--- setup_and_seed.py
+++ setup_and_seed.py
@@ -5,21 +5,24 @@ sys.path.append(os.path.join(os.path.realpath(os.path.dirname(__file__)), "..", 
 django.setup()
 
 from techtest.articles.models import Article
+from techtest.authors.models import Author
 from techtest.regions.models import Region
 from django.core import management
 
 # Migrate
 management.call_command("migrate", no_input=True)
 # Seed
+homer = Author.objects.create(first_name="Homer", last_name="Simpson")
+marge = Author.objects.create(first_name="Marge", last_name="Simpson")
 Article.objects.create(title="Fake Article", content="Fake Content").regions.set(
     [
         Region.objects.create(code="AL", name="Albania"),
         Region.objects.create(code="UK", name="United Kingdom"),
     ]
 )
+Article.objects.create(title="Fake Article", content="Fake Content", author=marge)
 Article.objects.create(title="Fake Article", content="Fake Content")
-Article.objects.create(title="Fake Article", content="Fake Content")
-Article.objects.create(title="Fake Article", content="Fake Content")
+Article.objects.create(title="Fake Article", content="Fake Content", author=homer)
 Article.objects.create(title="Fake Article", content="Fake Content").regions.set(
     [
         Region.objects.create(code="AU", name="Austria"),
